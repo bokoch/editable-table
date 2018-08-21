8841e5ed2f69d40d1c74e9b2559f2107e3f4fbe3
bokoch <bokoch1995@gmail.com>
2018-08-21 10:27:37 +0300

    Fix control elements

--- js/main.js
+++ js/main.js
@@ -44,12 +44,36 @@
             this.renderTable(editableTableContainer, cols, rows);
             shadow.appendChild(editableTableContainer);
 
+
+            var rootTable = editableTableContainer.querySelector('#root-table');
+            var tds = rootTable.getElementsByTagName('td');
+
+            editableTableContainer.addEventListener('mouseout', function (e) {
+                console.log(e.target)
+                // editableTableContainer.querySelector('#control-row').style.display = "none";
+                // editableTableContainer.querySelector('#control-col').style.display = "none";
+            });
+
+            for (var i = 0; i < tds.length; i++) {
+                tds[i].addEventListener('mouseover', function () {
+                    editableTableContainer.querySelector('#control-row').style.display = "flex";
+                    editableTableContainer.querySelector('#control-col').style.display = "flex";
+
+                    editableTableContainer.querySelector('#control-col').style.marginLeft = (this.offsetLeft + 72) + "px";
+                    editableTableContainer.querySelector('#control-row').style.marginTop = this.offsetTop + "px";
+
+                    editableTableContainer.querySelector('#control-col').style.width = this.offsetWidth + "px";
+                    editableTableContainer.querySelector('#control-row').style.height = this.offsetHeight + "px";
+                });
+            }
+
         }
 
         getStyles(element) {
             element.innerHTML = `
                 <style>
                     table {
+                        border: 2px solid #5d90e2;
                     }
                     td {
                         height: 70px;
@@ -59,15 +83,27 @@
                         color: white;
                         padding: 0 10px;
                     }
-                    tr:first-child td, td:first-child {
-                        background-color: #d85147;
+                    .control-col {
+
+                    }
+                    .control-row, .control-col {
+                        width: 70px;
+                        height: 70px;
+                        background-color: #cc4543;
+                        color: white;
+                        justify-content: center;
+                        align-items: center;
+                        display: none;
                         font-size: 30px;
                     }
-                    .control-cell {
-                        
+                    .control-col td, .control-row td {
+                        background-color: #bf563f;
                     }
-                    .control-row {
-                        
+                    .row-container {
+                    display: flex;;
+                    }
+                    .main-container {
+                    
                     }
                 </style>
             `;
@@ -75,30 +111,47 @@
 
         // element - dom element where table will be attached
         renderTable(element, colNumber, rowNumber) {
+
+            var mainContainer = document.createElement("DIV");
+            mainContainer.classList.add('main-container');
+            var rowContainer = document.createElement("DIV");
+            rowContainer.classList.add('row-container');
+
+            // Sub table for removing rows
+            var controlCol = document.createElement("DIV");
+            controlCol.classList.add('control-col');
+            controlCol.id = 'control-col';
+            controlCol.textContent = "-";
+
+            // Sub table for removing rows
+            var controlRow = document.createElement("DIV");
+            controlRow.classList.add('control-row');
+            controlRow.id = 'control-row';
+            controlRow.textContent = "-";
+
+
             // Main table
             var table = document.createElement("TABLE");
-            table.classList.add('root-table');
+            table.id = 'root-table';
 
-            for (var i = 0; i < rowNumber + 1; i++) {
+            for (var i = 0; i < rowNumber; i++) {
                 var tr = document.createElement('TR');
-                if (i === 0 ) {
-                    tr.classList.add('control-row')
-                }
-                for (var j = 0; j < colNumber + 1; j++) {
+                for (var j = 0; j < colNumber; j++) {
                     var td = document.createElement('TD');
-                    if (i === 0 || j === 0) {
-                        td.textContent = "-";
-                        td.classList.add('control-cell')
-                    } else {
-                        td.textContent = "Test";
-                    }
+                    td.textContent = "Test";
                     tr.appendChild(td);
                 }
                 table.appendChild(tr);
             }
 
             // Main table
-            element.appendChild(table);
+            rowContainer.appendChild(controlRow);
+            rowContainer.appendChild(table);
+
+            mainContainer.appendChild(controlCol);
+            mainContainer.appendChild(rowContainer);
+
+            element.appendChild(mainContainer);
         }
 
         // Get rows number of table from attributes
